689eef1dda830699e051c31acbee9fd86473ce38
KuyaLoy <robintapiru0894@gmail.com>
2024-06-29 16:43:39 +0400

    More Refactoring Job Details Component &  Improving Fetch Calls Good Structure For fetch Calls 2xx 4xx Status Codes

--- src/common.js
+++ src/common.js
@@ -1,3 +1,8 @@
+// Constant
+export const BASE_API_URL = "https://bytegrad.com/course-assets/js/2/api";
+export const DEFAULT_DISPLAY_TIME = 3500;
+
+// Selector
 export const bookmarksBtnEl = document.querySelector(".bookmarks-btn");
 export const errorEl = document.querySelector(".error");
 export const errorTextEl = document.querySelector(".error__text");
